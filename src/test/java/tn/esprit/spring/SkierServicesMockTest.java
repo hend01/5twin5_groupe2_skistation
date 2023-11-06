4d4091907d4967448f4be34dbc1d8f77108b1b11
bensaid.mohamedamine@esprit.tn <92233451+aminebensaid10@users.noreply.github.com>
2023-11-06 17:09:43 +0100

    dockerfilee

---
+++ src/test/java/tn/esprit/spring/SkierServicesMockTest.java
@@ -0,0 +1,80 @@
+package tn.esprit.spring;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import tn.esprit.spring.entities.Skier;
+import tn.esprit.spring.entities.Subscription;
+import tn.esprit.spring.entities.TypeSubscription;
+import tn.esprit.spring.repositories.ISkierRepository;
+import tn.esprit.spring.repositories.ISubscriptionRepository;
+import tn.esprit.spring.services.SkierServicesImpl;
+//import tn.esprit.spring.Types.TypeSubscription;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+public class SkierServicesMockTest {
+    @InjectMocks
+    private SkierServicesImpl skierService;
+
+    @Mock
+    private ISkierRepository skierRepository;
+
+    @Mock
+    private ISubscriptionRepository subscriptionRepository;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+
+        Skier skier = new Skier();
+        skier.setNumSkier(1L);
+        skier.setFirstName("John");
+        skier.setLastName("Doe");
+        skier.setDateOfBirth(LocalDate.of(1998, 5, 15));
+        skier.setCity("New York");
+
+        Subscription subscription = new Subscription();
+        subscription.setTypeSub(TypeSubscription.ANNUAL);
+        subscription.setStartDate(LocalDate.now());
+
+        skier.setSubscription(subscription);
+
+        when(skierRepository.save(skier)).thenReturn(skier);
+    }
+
+
+    @Test
+    public void testAddSkier() {
+        Skier skier = new Skier();
+        Subscription subscription = new Subscription();
+        subscription.setTypeSub(TypeSubscription.ANNUAL);
+        subscription.setStartDate(LocalDate.now());
+
+        skier.setSubscription(subscription);
+
+        when(skierRepository.save(skier)).thenReturn(skier);
+
+        Skier result = skierService.addSkier(skier);
+
+        assertEquals(skier, result);
+    }
+
+    @Test
+    public void testRetrieveAllSkiers() {
+        List<Skier> skiers = new ArrayList<>();
+        when(skierRepository.findAll()).thenReturn(skiers);
+
+        List<Skier> result = skierService.retrieveAllSkiers();
+
+        assertEquals(skiers, result);
+    }
+
+
+}
