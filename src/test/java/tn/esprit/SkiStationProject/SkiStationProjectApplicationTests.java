80188bb97a22567a1199df00401871c164edf2c4
Moetaz-doghman <61354236+Moetaz-doghman@users.noreply.github.com>
2023-11-12 21:56:19 +0100

    Update SkiStationProjectApplicationTests.java

--- src/test/java/tn/esprit/SkiStationProject/SkiStationProjectApplicationTests.java
+++ src/test/java/tn/esprit/SkiStationProject/SkiStationProjectApplicationTests.java
@@ -3,7 +3,7 @@ package tn.esprit.SkiStationProject;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class SkiStationProjectApplicationTests {
 
     @Test
