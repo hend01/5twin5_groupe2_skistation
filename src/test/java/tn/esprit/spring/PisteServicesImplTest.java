ef4bf38e45ba81101ab7f1010d72699f1f49a7e4
krichen-Noureddine <noureddine.krichen@esprit.tn>
2023-11-14 04:48:18 +0100

    Fixing

--- src/test/java/tn/esprit/spring/PisteServicesImplTest.java
+++ src/test/java/tn/esprit/spring/PisteServicesImplTest.java
@@ -1,40 +1,27 @@
 package tn.esprit.spring;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import tn.esprit.spring.entities.Color;
 import tn.esprit.spring.entities.Piste;
 import tn.esprit.spring.entities.Skier;
 import tn.esprit.spring.repositories.IPisteRepository;
 import tn.esprit.spring.repositories.ISkierRepository;
-import tn.esprit.spring.services.IPisteServices;
 import tn.esprit.spring.services.PisteServicesImpl;
-
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-@Configuration
+
 @ExtendWith(MockitoExtension.class)// Use MockitoExtension for JUnit 5
 class PisteServicesImplTest {
 
     @Mock
     IPisteRepository  pisteRepository;
-    @Mock
-    ISkierRepository skierRepository;
     @InjectMocks
     PisteServicesImpl pisteServices;
     Piste piste7 = new Piste(7L, "Slope7", Color.RED,500,20, new HashSet<Skier>());
@@ -51,12 +38,9 @@ class PisteServicesImplTest {
         when(pisteRepository.save(piste7)).thenReturn(piste7);
         Piste result = pisteServices.addPiste(piste7);
         assertNotNull(result);
-
-
     }
 
     @Test
-
     void retrieveAllPistes() {
         // Simuler le comportement de pisteRepository.findAll()
         when(pisteRepository.findAll()).thenReturn(listPiste);
